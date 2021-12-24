4ee3c482aaac0ce08e4768825e45517544f11b20
tl-kieran-allen <kieran.allen@truelayer.com>
2021-12-24 14:47:24 +0000

    sort errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Main } from "./Main";
+import { Main } from "../components/Main";
 
 const Home: NextPage = () => {
   return (
