8968e5746278ae3371e96c6caadd93e8e35750ca
tl-kieran-allen <kieran.allen@truelayer.com>
2021-12-24 14:40:33 +0000

    escape char

--- pages/Summary.tsx
+++ pages/Summary.tsx
@@ -7,7 +7,7 @@ export const Summary = () => {
       <p>
         Frontend Developer with a demonstrated history of working in the
         computer software industry. Skilled in JavaScript, ReactJS, React
-        Native, Redux, CSS, and HTML. Outside of work you'll find me cycling the
+        Native, Redux, CSS, and HTML. Outside of work you&apos;ll find me cycling the
         streets of London or hunkered down at home playing video games.
       </p>
     </section>
