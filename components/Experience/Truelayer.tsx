c69de40cf7748a5cd3938f1597846de66064e6de
Kieran Allen <kieran.allen@ovoenergy.com>
2022-08-27 01:50:10 +0100

    add links

--- components/Experience/Truelayer.tsx
+++ components/Experience/Truelayer.tsx
@@ -16,13 +16,28 @@ export function Truelayer() {
       </header>
       <ul>
         <li>
-          Helped build TrueLayers hosted payments page which now facilitates
-          &gt; 2000 transactions a day across Europe and the UK.
+          Helped build TrueLayers{" "}
+          <a
+            href="https://docs.truelayer.com/docs/hosted-payment-page-for-payments-v3"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted-payments-page
+          </a>{" "}
+          which now facilitates &gt; 2000 transactions a day across Europe and
+          the UK.
         </li>
         <li>
-          Helped build and publish the TrueLayers WebSDK which allows customers
-          to more seamlessly integrate an open banking payments flow into their
-          website.
+          Helped build and publish the{" "}
+          <a
+            href="https://www.npmjs.com/package/truelayer-web-sdk"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            truelayer-web-sdk
+          </a>{" "}
+          which allows customers to more seamlessly integrate an open banking
+          payments flow into their website.
         </li>
         <li>
           Worked on integrating MSW into the Truelayer hosted payments page
