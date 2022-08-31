7e475448abb5082718ef0b5a9cd393254161673e
Kieran Allen <kieran.allen@ovoenergy.com>
2022-08-31 15:44:09 +0100

    update summary

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -5,10 +5,15 @@ export const Summary = () => {
         <h3>Summary</h3>
       </header>
       <p>
-        Frontend Developer with a demonstrated history of working in the
-        computer software industry. Skilled in JavaScript, ReactJS, React
-        Native, Redux, CSS, and HTML. Outside of work you&apos;ll find me cycling the
-        streets of London or hunkered down at home playing video games.
+        I&apos;ve been working as a frontend developer for a number of years
+        now, and in that time I&apos;ve become skilled in a number of different
+        technologies. I have a strong foundation in JavaScript, and I&apos;m
+        also experienced with ReactJS, Redux, ReactQuery, TailwindsCSS and
+        NextJS. I&apos;m always looking to improve my skills and learn new
+        technologies, so that I can provide the best possible experience for my
+        users. Outside of work, I enjoy cycling and playing video games. I am
+        very interested in the potential that cryptocurrencies have to change
+        the way that we think about money and finance.
       </p>
     </section>
   );
